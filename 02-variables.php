ff5e56b3a1502957d67196dcace28f3606640b3e
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-06-03 01:57:31 -0400

    Update 02-variables.php

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,19 @@
 <?php include 'includes/header.php';
 
-$nombre = "Juan";
+$nombre = "Christian";
 
+$nombre = "Christian 2";
+
+echo $nombre;
+var_dump($nombre);
+
+define('constante', "Este es el valor de la constante");
+echo constante;
+
+const constante2 = "Hola 2";
+echo constante2;
+
+$nombreCliente = "Mary";
+$nombre_cliente = "Mary";
 
 include 'includes/footer.php';
