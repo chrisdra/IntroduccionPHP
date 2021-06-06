65fe85e330338027c3cb8101cb87055c813b11be
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-06-06 11:45:39 -0400

    Update final
    Se termina el repaso de PHP.

--- 13-loops.php
+++ 13-loops.php
@@ -1,6 +1,51 @@
 <?php include 'includes/header.php';
 
+//While
+$i = 0;
 
+while($i < 10):
+    echo $i . "<br>";
 
+    $i++; //Incremento
+endwhile;
+
+echo "<br>";
+// Do While
+$i = 0;
+
+do {
+    echo $i . "<br>";
+
+    $i++; //Incremento
+} while($i < 10);
+
+echo "<br>";
+//For Loop
+// for ($i = 0; $i < 1000; $i++ ) {
+//     if($i % 3 === 0 && $i % 5 === 0){
+//         echo $i . "- FIZZ BUZZ <br/>";
+//     } else if( $i % 3 === 0) {
+//         echo $i . "- Fizz <br/>";
+//     } else if($i % 5 === 0) {
+//         echo $i . "- Buzz <br/>";
+//     }
+// }
+
+echo "<br>";
+//For Each
+$clientes = array('Pedro', 'Juan', 'Karen');
+
+foreach( $clientes as $cliente) {
+    echo $cliente . "<br/>";
+}
+
+$cliente = [
+    'nombre' => 'Christian',
+    'saldo' => 200,
+    'tipo' => 'premium'
+];
+foreach($cliente as $key => $valor):
+    echo $key . " - " . $valor . '<br/>';
+endforeach;
 
 include 'includes/footer.php';
--- 17-require.php
+++ 17-require.php
@@ -1,6 +1,7 @@
 <?php include 'includes/header.php';
 
-
+require 'funciones.php';
+iniciarApp();
 
 
 
